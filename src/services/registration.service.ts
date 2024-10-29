7eb8bad9bbfe776bf4977d34dc40c27ffc7b95c0
EduardoMango <149683900+EduardoMango@users.noreply.github.com>
2024-10-29 13:24:50 -0300

    V 0.1
    Funcional el inicio de sesion, registro y persistencia de datos del usuario.

---
+++ src/services/registration.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {User} from '../Interfaces/User';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegistrationService {
+
+  private apiUsers = 'http://localhost:3000/usuarios';
+
+  constructor(private HttpClient: HttpClient) { }
+
+  registerUser(user: User): Observable<User>{
+    return this.HttpClient.post<User>(this.apiUsers, user);
+  }
+}
