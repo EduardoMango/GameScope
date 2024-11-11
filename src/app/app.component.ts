bb3974b3ecf16d0795698ef3ac1de7a1f164c372
LUCKMAX94 <lucasmaximilianopadilla@gmail.com>
2024-11-11 20:03:00 -0300

    API de juegos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MainComponent } from './main/main.component';
+import { SearchResultsComponent } from './games/components/search-results/search-results.component';
+import { GameSearchComponent } from './games/components/game-search/game-search.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainComponent],
+  imports: [RouterOutlet, SearchPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { TokenInterceptor } from './service/token-interceptor.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true }, provideHttpClient()]
 };
